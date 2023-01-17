011112083d9612c7f228dc8746668193049f3edf
Kiril Shamonin <oldshamoninkirill@gmail.com>
2023-01-17 17:42:12 +0200

    Changes to be committed: 	new file:   pages/All_Books_Pages.py
    Changes not staged for commit:
    	modified:   locators/all_books_page.py
    	modified:   locators/book_locators.py

---
+++ pages/All_Books_Pages.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+from locators.all_books_page import AllBooksPageLocators
+
+class AllBooksPage:
+    def __init__(self, page_content) -> None:
+        self.soup = BeautifulSoup(page_content, 'html.parser')
+
+    @property
+    def books(self):
+        return [BookParser (e) for e in self.soup.select(AllBooksPageLocators.BOOKS)]
