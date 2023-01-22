4f7d2efab8632f6ac4acf31cc543f006bd5fe0fa
oldkirillius <oldshamoninkirill@gmail.com>
2023-01-22 14:24:22 +0200

    Changes to be committed: 	modified:   __pycache__/app.cpython-311.pyc 	modified:   app.py 	modified:   locators/__pycache__/all_books_page.cpython-311.pyc 	modified:   locators/all_books_page.py 	modified:   pages/All_Books_Pages.py 	modified:   pages/__pycache__/All_Books_Pages.cpython-311.pyc

--- pages/All_Books_Pages.py
+++ pages/All_Books_Pages.py
@@ -1,6 +1,10 @@
+import re
 from bs4 import BeautifulSoup
+
 from locators.all_books_page import AllBooksPageLocators
 from parser.book_parser import BookParser
+
+
 
 class AllBooksPage:
     def __init__(self, page_content) -> None:
@@ -9,3 +13,11 @@ class AllBooksPage:
     @property
     def books(self):
         return [BookParser(e) for e in self.soup.select(AllBooksPageLocators.BOOKS)]
+
+    @property
+    def page_count(self):
+        content = self.soup.select_one(AllBooksPageLocators.PAGER).string
+        pattern = 'Page [0-9]+ of ([0-9]+)'
+        matcher = re.search(pattern, content)
+        pages = int(matcher.group(1))
+        return pages
