7112d0ee202a1aa3da351db5fdeac60624e2bc66
oldkirillius <oldshamoninkirill@gmail.com>
2023-01-22 15:12:11 +0200

    Changes to be committed: 	modified:   app.py
    Changes not staged for commit:
    	modified:   __pycache__/app.cpython-311.pyc
    	modified:   pages/All_Books_Pages.py
    	modified:   pages/__pycache__/All_Books_Pages.cpython-311.pyc
    	modified:   parser/__pycache__/book_parser.cpython-311.pyc
    	modified:   parser/book_parser.py
    
    Untracked files:
    	logs.txt

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import requests
+import logging
 
 from pages.All_Books_Pages import AllBooksPage
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S',
+                    level=logging.DEBUG,
+                    filename='logs.txt')
+
+logger = logging.getLogger('scraping')
+
+print('Loading books list...')
+logger.info('Loading books list.')
+
+logger.info('Requesting http://books.toscrape.com')
 page_content = requests.get('https://books.toscrape.com').content
 page = AllBooksPage(page_content)
 
@@ -10,5 +23,6 @@ books = page.books
 for page_num in range(1, page.page_count):
     url=f'https://books.toscrape.com/catalogue/page-{page_num+1}.html'
     page_content = requests.get(url).content
+    logger.debug('Creating AllBooksPage from page content.')
     page = AllBooksPage(page_content)
     books.extend(page.books)
