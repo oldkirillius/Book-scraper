de5d5845903bc1972e1d3d4bff7693e1329acc36
oldkirillius <oldshamoninkirill@gmail.com>
2023-01-22 14:08:20 +0200

    Changes to be committed: 	new file:   __pycache__/app.cpython-311.pyc 	modified:   app.py 	new file:   locators/__pycache__/all_books_page.cpython-311.pyc 	new file:   locators/__pycache__/book_locators.cpython-311.pyc 	modified:   menu.py 	new file:   pages/__pycache__/All_Books_Pages.cpython-311.pyc 	new file:   parser/__pycache__/book_parser.cpython-311.pyc

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import requests
 
 from pages.All_Books_Pages import AllBooksPage
-
 page_content = requests.get('https://books.toscrape.com').content
 page = AllBooksPage(page_content)
 
+
 books = page.books
 
+for page_num in range(1, 50):
+    url=f'https://books.toscrape.com/catalogue/page-{page_num+1}.html'
+    page_content = requests.get(url).content
+    page = AllBooksPage(page_content)
+    books.extend(page.books)
--- menu.py
+++ menu.py
@@ -1,5 +1,16 @@
 from app import books
 
+USER_CHOICE = '''Enter one of the following options:
+
+- 'b' to look at 5-star books
+- 'c' to look at the cheapest books
+- 'n' to look at next available book
+- 'q' to exit
+
+Enter your choice: '''
+
+
+
 def print_best_books():
     best_books = sorted(books, key=lambda x: x.rating)
     for book in best_books:
@@ -11,7 +22,26 @@ def print_cheapest_books():
     for book in cheapest_books:
         print (book)
 
+books_generator = (x for x in books)
 
-print_best_books()
+def print_next_book():
+    print(next(books_generator))
 
-print_cheapest_books()
+user_choises = {
+    'b': print_best_books,
+    'c': print_cheapest_books,
+    'n':print_next_book
+
+}
+
+def menu():
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        if user_input in ('b', 'c', 'n'):
+            user_choises[user_input]()
+
+        else:
+            print ('Choose the valid command')
+        user_input = input(USER_CHOICE)
+
+menu()
