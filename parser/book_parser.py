e07dad2769fbec734ba50c9c15cd5d8e64af63f1
oldkirillius <oldshamoninkirill@gmail.com>
2023-01-22 15:12:54 +0200

    Changes to be committed: 	modified:   __pycache__/app.cpython-311.pyc 	new file:   logs.txt 	modified:   pages/All_Books_Pages.py 	modified:   pages/__pycache__/All_Books_Pages.cpython-311.pyc 	modified:   parser/__pycache__/book_parser.cpython-311.pyc 	modified:   parser/book_parser.py

--- parser/book_parser.py
+++ parser/book_parser.py
@@ -1,7 +1,9 @@
 import re
+import logging
 
 from locators.book_locators import BookLocators
 
+logger = logging.getLogger('scraping.book_parser')
 
 class BookParser:
     RATINGS = {
@@ -14,6 +16,7 @@ class BookParser:
     
     
     def __init__(self, parent):
+        logger.debug(f'New book parser created from `{parent}`')
         self.parent = parent
 
     def __repr__(self):
@@ -21,28 +24,38 @@ class BookParser:
 
     @property
     def name(self):
+        logger.debug('Finding book name...')
         locator = BookLocators.NAME_LOCATOR
         item_link = self.parent.select_one(locator)
+        logger.info(f'Found book name, `{item_name}`.')
         item_name = item_link.attrs['title']
         return item_name
 
     @property
     def link(self):
+        logger.debug('Finding book page link...')
         locator = BookLocators.LINK_LOCATOR
         item_link = self.parent.select_one(locator).attrs['href']
+        logger.info(f'Found book page link, `{item_link}`.')
         return item_link
 
     @property
     def price(self):
+        logger.debug('Finding book price...')
         locator = BookLocators.PRICE_LOCATOR
         item_price = self.parent.select_one(locator).string
+        logger.debug(f'Item price element found, `{item_price}`')
 
         pattern = '£([0-9]+\.[0-9]+)'
         matcher = re.search(pattern, item_price)
-        return float(matcher.group(1))
+        
+        price = float(matcher.group(1))
+        logger.info(f'Found book price, `{price}`.')
+        return price
 
     @property
     def rating(self):
+        logger.debug('Finding book rating...')
         locator = BookLocators.RATING_LOCATOR
         star_rating_tag = self.parent.select_one(locator)
         classes = star_rating_tag.attrs['class']
